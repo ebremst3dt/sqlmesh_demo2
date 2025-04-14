c8745df89d0f452020279091e7c78a5765f75694
Erik Bremstedt <erik.bremstedt@gmail.com>
2025-04-14 10:33:27 +0200

    readme

--- data_load_parameters/takecare.py
+++ data_load_parameters/takecare.py
@@ -1,6 +1,6 @@
 from data_load_parameters.helpers import start_of_year_minus_x_years
 
 
-start = "2024-04-01"
+start = "2025-04-13"
 #start = start_of_year_minus_x_years(years_to_subtract=3)
 start_vcodes = start_of_year_minus_x_years(years_to_subtract=15)
